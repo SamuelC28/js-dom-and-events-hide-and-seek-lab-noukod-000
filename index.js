77e229652f6098340a2c07dc4fb548e5b0c4469c
Samuel Cornet <samuel@noukod.com>
2019-02-12 19:07:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 function getFirstSelector(selector) {
-  return document.getElementsByTagName('selector')[0]
-  //return{document.querySelector('selector')}
+  return document.querySelector('selector')
 }
